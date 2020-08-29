a52a2a1679f0a87911d8a618355e44c79a251283
dasbairagya <gopal.dasbairagya@ingrammicro.com>
2020-08-29 19:24:16 +0530

    updated

--- html.php
+++ html.php
@@ -25,13 +25,7 @@
 // print_r($blogs);
 // die();
 
-
-
-
 ?>
-
-
-
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
